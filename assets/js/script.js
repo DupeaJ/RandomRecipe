715941b6e4cf4ce8a209d69d1afc5354d818c18e
manc1n1 <mancinij1111@gmail.com>
2023-07-12 21:53:33 -0400

    Updated script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,3 @@
-const callBtn = document.querySelector("#call-button");
-//selecting button
-
-var Url = "https://www.themealdb.com/api/json/v1/1/categories.php?";
-// url variable
-
-const categoriesElement = document.getElementById("categoryDescription");
-
-function filterCall() {
-    const filterUrl =
-        "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast";
-    fetch(filterUrl)
-        //call api return response as json
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.meals);
-        });
-}
-
-function callRecipeApi() {
-    fetch(Url)
-        //call api return response as json
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            const categories = data.categories;
-            //selecting categories tag
-            categories.forEach(function (category) {
-                //for each category the following will happen for each
-                const strCategory = category.strCategory;
-                const categoryElement = document.createElement("p");
-                //create element
-                categoryElement.textContent = strCategory;
-                //set text
-                categoriesElement.appendChild(categoryElement);
-                //append to page
-            });
-        })
-        .catch((error) => {
-            console.log(error);
-            //if error console log what the error is
-        });
-}
-
-callBtn.addEventListener("click", callRecipeApi);
-callBtn.addEventListener("click", filterCall);
-//event listener on btn to call recipeapi
-
 $(function () {
     var hamburgerMenu = $("#hamburger-menu");
     var closeBtn = $("#close-btn");
@@ -60,4 +9,55 @@ $(function () {
     closeBtn.on("click", function () {
         $("#side-bar").attr("style", "width: 0px;");
     });
+
+    const callBtn = document.querySelector("#call-button");
+    //selecting button
+
+    var Url = "https://www.themealdb.com/api/json/v1/1/categories.php?";
+    // url variable
+
+    const categoriesElement = document.getElementById("categoryDescription");
+
+    function filterCall() {
+        const filterUrl =
+            "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast";
+        fetch(filterUrl)
+            //call api return response as json
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data.meals);
+            });
+    }
+
+    function callRecipeApi() {
+        fetch(Url)
+            //call api return response as json
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const categories = data.categories;
+                //selecting categories tag
+                categories.forEach(function (category) {
+                    //for each category the following will happen for each
+                    const strCategory = category.strCategory;
+                    const categoryElement = document.createElement("p");
+                    //create element
+                    categoryElement.textContent = strCategory;
+                    //set text
+                    categoriesElement.appendChild(categoryElement);
+                    //append to page
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                //if error console log what the error is
+            });
+    }
+
+    callBtn.addEventListener("click", callRecipeApi);
+    callBtn.addEventListener("click", filterCall);
+    //event listener on btn to call recipeapi
 });
