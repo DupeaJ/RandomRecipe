750f8dfa04997fd4202c57de3facd50a4be4b714
DupeaJ <127266733+DupeaJ@users.noreply.github.com>
2023-07-17 17:13:51 -0700

    makes choice random based on user choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,14 +22,54 @@ $(function () {
     const generateBtn = document.querySelector(".btn-to-spin");
     //selecting button
 
+function callRecipeApi() {
+        fetch(Url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                const categories = data.categories;
+                categories.forEach(function (category) {
+                    const strCategory = category.strCategory;
+                    const labelElement = document.createElement("label");
+                    const categoryElement = document.createElement("input");
+
+                    categoryElement.type = "checkbox";
+                    categoryElement.value = strCategory;
+
+                    const categoryNameElement = document.createElement("span");
+                    categoryNameElement.textContent = strCategory;
+
+                    labelElement.appendChild(categoryElement);
+                    labelElement.appendChild(categoryNameElement);
+                    categoriesElement.appendChild(labelElement);
+
+                    document.querySelector(".big-btn").style.display = "none";
+
+                    
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+}
+    
     var Url = "https://www.themealdb.com/api/json/v1/1/categories.php?";
     // url variable
 
     const categoriesElement = document.getElementById("categoryDescription");
 
     function filterCall() {
+        const checkboxes = document.querySelectorAll(
+        'input[type="checkbox"]:checked'
+    );
+    const checkedValues = Array.from(checkboxes).map(
+        (checkbox) => checkbox.value
+    );
+
         const filterUrl =
-            "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast";
+            "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + checkedValues.join(",");
+        
         fetch(filterUrl)
             //call api return response as json
             .then(function (response) {
@@ -62,46 +102,20 @@ $(function () {
 
     }
 
-    function callRecipeApi() {
-        fetch(Url)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                const categories = data.categories;
-                categories.forEach(function (category) {
-                    const strCategory = category.strCategory;
-                    const labelElement = document.createElement("label");
-                    const categoryElement = document.createElement("input");
-
-                    categoryElement.type = "checkbox";
-                    categoryElement.value = strCategory;
-
-                    const categoryNameElement = document.createElement("span");
-                    categoryNameElement.textContent = strCategory;
-
-                    labelElement.appendChild(categoryElement);
-                    labelElement.appendChild(categoryNameElement);
-                    categoriesElement.appendChild(labelElement);
-
-                    document.querySelector(".big-btn").style.display = "none";
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
+    
 
     function generateMeal() {
         fetch(Url)
             .then(function (response) {
                 return response.json();
             });
-        document.querySelector("#categoryDescription").style.display = "none";
+            document.querySelector("#categoryDescription").style.display = "none";
+        
+                
     }
 
     callBtn.addEventListener("click", callRecipeApi);
-    callBtn.addEventListener("click", filterCall);
+    generateBtn.addEventListener("click", filterCall);
     generateBtn.addEventListener("click", generateMeal);
 
     //event listener on btn to call recipeapi
